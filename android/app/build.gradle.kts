8e05a363a47ff351c1bf0b5f098b327020ed6cd5
celilyigit <celil.yigit@tum.de>
2025-05-15 20:21:32 +0200

    NDK update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "celilygt.in_your_face"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "27.2.12479018"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
